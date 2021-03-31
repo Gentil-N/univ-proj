f889e9128f99d686a50cf845b97c04ad8833a598
Gentil-N <ngentil77@gmail.com>
2021-03-31 11:07:33 +0200

    begin collide

--- code/ball.py
+++ code/ball.py
@@ -11,6 +11,24 @@ Wind = None
 
 gravity = [0.0, -sc_const.g]
 
+
+def normalize(vec):
+    norm = math.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
+    return [vec[0]/norm, vec[1]/norm]
+
+def dot(vec1, vec2):
+    return vec1[0] * vec2[0] + vec1[1] * vec2[1]
+
+def to_vec(point1, point2):
+    return [point2[0] - point1[0], point2[1] - point1[1]]
+
+def new_pos(deltat, old_speed):
+    return deltat * old_speed
+
+def new_speed(deltat, old_speed, coeff1, coeff2, mass):
+    return deltat * (gravity + (coeff1/mass + coeff2/mass * abs(old_speed)) * old_speed)
+
+
 class FlatWall(game.BaseFlatWall):
     def intersect(self, p1, p2, r):
         '''
@@ -22,6 +40,17 @@ class FlatWall(game.BaseFlatWall):
         self.P un point qui appartient au plan du mur
         self.N un vecteur normal (sortant) du mur
         '''
+        d = normalize([-self.N[1], self.N[0]])
+        v = normalize(to_vec(p1, p2))
+        det = v[0] * d[1] - v[1] * d[0]
+        if det == 0 :
+            return [False, [0, 0], [0, 0]]
+        b = (d[1] * (self.P[0] - p1[0]) - d[0] * (self.P[1] + p1[1])) / det
+        if b < 0 :
+            return [False, [0, 0], [0, 0]]
+        k = p1 + b * v
+        theta = math.sin(math.acos(dot(v, d)))
+        #to be continued...
         return None
 
 
@@ -39,7 +68,7 @@ class Ball(game.BaseBall):
         '''
         super().__init__(R, P0, dP0, O0, dO0)
         self.k1 = -6 * math.pi * self.R * eta
-        self.k2 = -cx * rho * math.pi * self.R * self.R
+        self.k2 = 0  # -cx * rho * math.pi * self.R * self.R
         self.M = M
         self.I = 2 * self.M * self.R ** 2 / 5
 
@@ -52,6 +81,5 @@ class Ball(game.BaseBall):
 
         On utilisera la liste Bounds pour connaitre l'ensemble des murs du problème.
         '''
-        self.P += deltat * self.dP
-        self.dP += deltat * (gravity + (self.k1/self.M + self.k2/self.M * abs(self.dP)) * self.dP)
-        #self.dP[1] += deltat * (gravity[1] + (self.k1/self.M + self.k2/self.M * abs(self.dP[1])) * self.dP[1])
+        self.P += new_pos(deltat, self.dP)
+        self.dP += new_speed(deltat, self.dP, self.k1, self.k2, self.M)
